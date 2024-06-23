f2269a0ed1263e0f3e47421b86dc67992b862e41
ootoro29 <gobou256@gmail.com>
2024-06-23 15:09:42 +0900

    グループ追加機能追加

--- app/group/newgroup/page.tsx
+++ app/group/newgroup/page.tsx
@@ -1,8 +1,25 @@
 "use client"
 import Group from "@/features/layouts/group"
+import { useState } from "react";
 
 export default function NewGroupPage() {
+    const [groupName,setGroupName] = useState("");
+    const handleCreateGroup = async() => {
+        const res = await fetch("/api/group",{
+            method:"POST",
+            headers: {
+                'Content-Type':"application/json"
+            },
+            body: JSON.stringify({name:groupName})
+        });
+        const data = await res.json();
+        setGroupName("");
+    }
     return(
-        <Group.NewGroupPage></Group.NewGroupPage>
+        <Group.NewGroupPage
+            handleCreateGroup = {handleCreateGroup}
+            groupName = {groupName}
+            setGroupName = {setGroupName}
+        ></Group.NewGroupPage>
     );
 }
--- features/layouts/group/NewGroup/new-group-page.tsx
+++ features/layouts/group/NewGroup/new-group-page.tsx
@@ -1,7 +1,29 @@
-export default function NewGroupPage() {
+import { Dispatch, SetStateAction } from "react";
+
+export default function NewGroupPage(
+    {
+        handleCreateGroup,
+        groupName,
+        setGroupName
+    }:{
+        handleCreateGroup:() => void,
+        groupName:string,
+        setGroupName:Dispatch<SetStateAction<string>>
+    }
+) {
     return(
         <div>
             <p>new group</p>
+            <form onSubmit={handleCreateGroup}>
+                <input 
+                    type="text"
+                    value={groupName}
+                    onChange={(e) => {
+                        setGroupName(e.target.value);
+                    }}
+                />
+                <button type="submit">送信</button>
+            </form>
         </div>
     );
 }
