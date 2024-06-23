d4bb285f757c727de33d468fc07416dd66fc1232
ootoro29 <gobou256@gmail.com>
2024-06-23 13:46:13 +0900

    トップページにボタン追加

--- features/layouts/top/TopPageLoggedIn/top-page-logged-in.tsx
+++ features/layouts/top/TopPageLoggedIn/top-page-logged-in.tsx
@@ -1,4 +1,5 @@
 import { auth, signOut } from "@/lib/auth";
+import Link from "next/link";
 
 export default async function TopPageLoggedIn() {
     const session = await auth();
@@ -12,6 +13,9 @@ export default async function TopPageLoggedIn() {
                 <button>サインアウト</button>
             </form>
             <p>{JSON.stringify(session,null,2)}</p>
+            <Link href={"/group/newgroup"}><button>グループ作成</button></Link>
+            <Link href={"/#"}><button>グループチャット入場</button></Link>
+            <Link href={"/#"}><button>コミュニティ入場</button></Link>
         </div>
     );    
 }
